f2024d4f599dddc4b720b4d2d1a091aaf9a1bfcf
Mazene <mmazenezerguine@gmail.com>
2025-03-21 17:56:43 +0100

    communication protocole

---
+++ networking/src/lib.rs
@@ -0,0 +1,9 @@
+mod fragment_request;
+mod fragment_task;
+mod fragment_result;
+
+pub use fragment_result::FragmentResultBuilder;
+pub use fragment_task::FragmentTask;
+pub use fragment_request::FragmentRequestBuilder;
+pub use fragment_result::FragmentResult;
+pub use fragment_request::FragmentRequest;
